347089ec3e1c64be902e6c3c143b81dc48f678a1
Sandriita <Sandriita@SANDRA>
2018-12-07 19:59:37 +0100

    Prueba Chuck

---
+++ PruebaChuck/PruebaChuck/Test.Designer.cs
@@ -0,0 +1,196 @@
+﻿namespace PruebaChuck
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buscar = new System.Windows.Forms.Button();
+            this.random = new System.Windows.Forms.CheckBox();
+            this.category = new System.Windows.Forms.ComboBox();
+            this.freeText = new System.Windows.Forms.TextBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.language = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // buscar
+            // 
+            this.buscar.Location = new System.Drawing.Point(436, 51);
+            this.buscar.Name = "buscar";
+            this.buscar.Size = new System.Drawing.Size(75, 23);
+            this.buscar.TabIndex = 0;
+            this.buscar.Text = "Search";
+            this.buscar.UseVisualStyleBackColor = true;
+            this.buscar.Click += new System.EventHandler(this.buscar_Click);
+            // 
+            // random
+            // 
+            this.random.AutoSize = true;
+            this.random.Location = new System.Drawing.Point(237, 55);
+            this.random.Name = "random";
+            this.random.Size = new System.Drawing.Size(66, 17);
+            this.random.TabIndex = 1;
+            this.random.Text = "Random";
+            this.random.UseVisualStyleBackColor = true;
+            this.random.CheckedChanged += new System.EventHandler(this.random_CheckedChanged);
+            // 
+            // category
+            // 
+            this.category.FormattingEnabled = true;
+            this.category.Items.AddRange(new object[] {
+            "explicit",
+            "dev",
+            "movie",
+            "food",
+            "celebrity",
+            "science",
+            "sport",
+            "political",
+            "religion",
+            "animal",
+            "history",
+            "music",
+            "travel",
+            "career",
+            "money",
+            "fashion"});
+            this.category.Location = new System.Drawing.Point(91, 17);
+            this.category.Name = "category";
+            this.category.Size = new System.Drawing.Size(121, 21);
+            this.category.TabIndex = 2;
+            this.category.SelectedIndexChanged += new System.EventHandler(this.category_SelectedIndexChanged);
+            // 
+            // freeText
+            // 
+            this.freeText.Location = new System.Drawing.Point(90, 52);
+            this.freeText.Name = "freeText";
+            this.freeText.Size = new System.Drawing.Size(122, 20);
+            this.freeText.TabIndex = 3;
+            this.freeText.TextChanged += new System.EventHandler(this.freeText_TextChanged);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 91);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(499, 148);
+            this.dataGridView1.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 20);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(52, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Category:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 56);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(51, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Free text:";
+            // 
+            // language
+            // 
+            this.language.FormattingEnabled = true;
+            this.language.Items.AddRange(new object[] {
+            "English",
+            "Spanish"});
+            this.language.Location = new System.Drawing.Point(298, 17);
+            this.language.Name = "language";
+            this.language.Size = new System.Drawing.Size(121, 21);
+            this.language.TabIndex = 7;
+            this.language.SelectedIndexChanged += new System.EventHandler(this.language_SelectedIndexChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(234, 20);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(58, 13);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Language:";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(436, 15);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 9;
+            this.button1.Text = "Email";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.email_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.ClientSize = new System.Drawing.Size(521, 252);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.language);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.freeText);
+            this.Controls.Add(this.category);
+            this.Controls.Add(this.random);
+            this.Controls.Add(this.buscar);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Test";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buscar;
+        private System.Windows.Forms.CheckBox random;
+        private System.Windows.Forms.ComboBox category;
+        private System.Windows.Forms.TextBox freeText;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox language;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ PruebaChuck/PruebaChuck/Test.cs
@@ -0,0 +1,184 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+using System.Data;
+using System.Globalization;
+
+namespace PruebaChuck
+{
+    public partial class Form1 : Form
+    {
+        CultureInfo myCIintl = new CultureInfo("en-GB", false);
+        public static Uri ProxyString { get; private set; }
+
+        //Entidad listas de frases
+        public class ListFacts
+        {
+            public List<Facts>[] result { get; set; }
+        }
+
+        //Entidad con las frases
+        public class Facts
+        {
+            public string [] Category { get; set; }
+            public string Icon_url { get; set; }
+            public string Url { get; set; }
+            public string Value { get; set; }
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        //Conexión con la API
+        public static string HttpGet(string URI)
+        {
+            System.Net.WebRequest req = System.Net.WebRequest.Create(URI);
+            req.Proxy = new System.Net.WebProxy(ProxyString, true); //true means no proxy
+            System.Net.WebResponse resp = req.GetResponse();
+            System.IO.StreamReader sr = new System.IO.StreamReader(resp.GetResponseStream());
+            return sr.ReadToEnd().Trim();
+        }
+
+        //Función botón buscar
+        private void buscar_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string URI = "";
+
+                //Validaciones
+                if (random.Checked)
+                {
+                    //Esto estaria mejor en un fichero config indicando las diferentes urls.
+
+                    //Fact Aleatorio
+                    URI = "https://api.chucknorris.io/jokes/random";
+                    string textJson = HttpGet(URI);
+
+                    Facts facts = new System.Web.Script.Serialization.JavaScriptSerializer().Deserialize<Facts>(textJson);
+                    LoadGrid(facts);
+                }
+                else if (!String.IsNullOrEmpty(category.Text))
+                {
+                    //Fact por categoria, el combo categorias podria cargarse de la API.
+                    URI = "https://api.chucknorris.io/jokes/random?category=" + category.Text;
+                    string textJson = HttpGet(URI);
+
+                    Facts facts = new System.Web.Script.Serialization.JavaScriptSerializer().Deserialize<Facts>(textJson);
+                    LoadGrid(facts);
+                }
+                else if (!String.IsNullOrEmpty(freeText.Text))
+                {
+                    //Fact por texto libre
+                    URI = "https://api.chucknorris.io/jokes/search?query=" + freeText.Text;
+                    string textJson = HttpGet(URI);
+
+                    //Nos devuelve mas de una linea
+                    ListFacts facts = new System.Web.Script.Serialization.JavaScriptSerializer().Deserialize<ListFacts>(textJson);
+
+                    //ListFacts Facts = new System.Web.Script.Serialization.JavaScriptSerializer().Deserialize<ListFacts>(HttpGet(URI));
+
+                    //foreach (var item in ListFacts)
+                    //{
+                    //    Console.WriteLine("Category: {0}, Icon_url: {1}, Url: {2}, Value: {3}", item.Category, item.Icon_url, item.Url, item.Value);
+                    //}
+                }
+                else
+                {
+                    MessageBox.Show("Debe seleccíonar un filtro.");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+        }
+
+        //Carga el grid
+        public void LoadGrid(Facts facts)
+        {
+            try
+            {
+                DataTable dt = new DataTable();
+                dt.Columns.Add("Facts");
+                DataRow row = dt.NewRow();
+                row["Facts"] = facts.Value;
+                dt.Rows.Add(row);
+
+                dataGridView1.DataSource = dt;
+                dataGridView1.AutoResizeColumns();
+                dataGridView1.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.AllCells;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+        }
+
+        private void random_CheckedChanged(object sender, EventArgs e)
+        {
+            if (random.Checked)
+            {
+                category.Text = "";
+                freeText.Text = "";
+            }
+            else
+            {
+                random.Checked = false;
+            }
+        }
+
+        private void category_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            // no vamos a permitir que se realicen busquedas random por categoria
+            random.Checked = false;
+            freeText.Text = "";
+        }
+
+        private void freeText_TextChanged(object sender, EventArgs e)
+        {
+            random.Checked = false;
+            category.Text = "";
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            myCIintl = new CultureInfo("en-GB", false);
+            language.SelectedIndex = 0;
+        }
+
+        //Cambiar idioma segun cultureinfo
+        private void language_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            switch (language.Text)
+            {
+                case "English":
+                    myCIintl = new CultureInfo("en-GB", false);
+                    label1.Text = "Category:";
+                    label2.Text = "Free text:";
+                    label3.Text = "Language:";
+                    random.Text = "Random";
+                    buscar.Text = "Search";
+                    button1.Text = "Email";
+                    break;
+                case "Spanish":
+                    myCIintl = new CultureInfo("es-ES", false);
+                    label1.Text = "Categoría:";
+                    label2.Text = "Texto Libre:";
+                    label3.Text = "Idioma:";
+                    random.Text = "Aleatorio";
+                    buscar.Text = "Buscar";
+                    button1.Text = "Correo";
+                    break;
+            }
+        }
+
+        private void email_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+
+}
